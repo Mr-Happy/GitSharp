23ef3553ff73080bbba6b8f66c4f457a649f8b1a
Mr_Happy <dvdvorle@gmail.com>
2010-01-03 14:34:45 +0100

    Added the GetOptionPart method.
    Also set the StubGenerator project as startupproject

--- StubGenerator/Program.cs
+++ StubGenerator/Program.cs
@@ -20,7 +20,11 @@ namespace StubGenerator
     /// </summary>
     public static class DocumentationParser
     {
-        
+        /// <summary>
+        /// Used for possible inclusions in the documentation.
+        /// </summary>
+        private static string directory;
+
         /// <summary>
         /// This method will try to parse one file and will return all
         /// options/arguments found.
@@ -29,8 +33,56 @@ namespace StubGenerator
         /// <returns>List of OptArgs containing all options/arguments found.</returns>
         public static List<OptArg> Parse(string file)
         {
+            directory = new FileInfo(file).DirectoryName;
+            List<string> optionPart = GetOptionPart(File.ReadAllLines(file));
+            
+            
+
+
 
             return new List<OptArg>();
+        }
+
+        /// <summary>
+        /// This method scans each line of the file-to-be-parsed, when it finds
+        /// the "OPTIONS" caption, it reads every following line into a new list.
+        /// Untill it finds the beginning of the next paragraph.
+        /// </summary>
+        /// <param name="content">The content of the file (usually File.ReadAllLines())</param>
+        /// <returns>a List containig each line of the option paragraph.</returns>
+        private static List<string> GetOptionPart(string[] content)
+        {
+            List<string> optionPart = new List<string>();
+
+            bool inContent = false;
+
+            // A line with 3 or more dashes denotes a new paragraph, and thus the end
+            // of the options paragraph.
+            for (int i = 0; !(inContent && content[i + 2].Contains("---")); i++)
+            {
+                if (inContent)
+                {
+                    optionPart.Add(content[i]);
+                }
+                if (content[i] == "OPTIONS" && content[i + 1] == "-------")
+                {
+                    inContent = true;
+                    i++;
+                }
+            }
+            
+            // Insert extra options if defined and found...
+            List<string> extraOptions = new List<string>();
+
+            foreach(string toBeIncluded in optionPart.Where<string>( o => o.StartsWith("include") && o.Contains("options")))
+            {
+                extraOptions.AddRange(File.ReadAllLines(directory + "\\" + toBeIncluded.Replace("[]", "").Replace("include::", "")));
+            }
+
+            optionPart.RemoveAll(o => o.StartsWith("include"));
+            optionPart.AddRange(extraOptions);
+            
+            return optionPart;
         }
     }
 
